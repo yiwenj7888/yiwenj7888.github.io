8b944b090b71a1ed14ff9db7758f89c124b20801
yiwenj7888 <YIwenj2@nycstudents.net>
2024-09-28 02:40:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Yiwen J."; // update the part BETWEEN the "quotes"
+let userName = "yiwenj7888"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
